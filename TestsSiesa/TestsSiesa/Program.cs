4fca298aaef282c3df69653d0352c4df4f25f7f5
guillermo2018 <41501110+guillermo2018@users.noreply.github.com>
2023-07-22 10:51:48 -0500

    TestsSiesa
    Dividir números enteros

---
+++ TestsSiesa/TestsSiesa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DivisionNumeros
+{
+	/// <summary>
+	/// Clase encargada Dividir numeros enteros
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Leer el dividendo
+			Console.Write("Ingrese el dividendo: ");
+			int dividendo = int.Parse(Console.ReadLine());
+
+			// Leer el divisor
+			Console.Write("Ingrese el divisor: ");
+			int divisor = int.Parse(Console.ReadLine());
+
+			// Realizar la división
+			if (divisor != 0)
+			{
+				double resultado = (double)dividendo / divisor;
+
+				// Mostrar el resultado
+				Console.WriteLine("El resultado de la división es: " + resultado);
+			}
+			else
+			{
+				Console.WriteLine("Error: No se puede dividir por cero.");
+			}
+		}
+	}
+}
